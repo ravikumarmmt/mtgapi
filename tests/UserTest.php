6ed57bce5e7e1c57f8ce7592071187843d9e3c8b
ravikumarmmt <ravikumarmmt@gmail.com>
2016-11-21 18:14:02 +0530

    checking data migration

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -12,7 +12,22 @@ class UserTest extends TestCase
         $response = $this->call('GET', '/users');
         $this->assertEquals(200, $response->status());
     }
-
+    public function testUserCreate(){
+        $api_token = str_random(60);
+        $results = DB::select(DB::raw('SELECT NOW() AS end_time'));
+        $user = User::create([  'name' => 'unittest',
+                                'email' => 'unittest@test.com',
+                                'password'=>  Crypt::encrypt('test@123'),
+                                'api_token' => $api_token,
+                                'expire_at' => $results[0]->end_time
+                            ]);
+         $this->seeInDatabase('users', ['email' => 'unittest@test.com']);
+    } 
+     public function testUserDelete(){
+        $user_found = User::where('email', 'unittest@test.com');
+        $user_found->delete();
+        $this->notSeeInDatabase('users', ['email' => 'unittest@test.com']);
+    }
    
     
 }
